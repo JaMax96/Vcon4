ce51a044e132cd4905850c4903ec1f3d9806bbc1
Jan Holzweber <j.holzweber@gmx.at>
2024-06-07 09:12:45 +0200

    added opponenet baseclass

---
+++ game/aiOpponent.py
@@ -0,0 +1,16 @@
+
+class EasyAI():
+
+    def getTurn(gamestate):
+        pass
+
+
+class MediumAI():
+    
+    def getTurn(gamestate):
+        pass
+
+class HardAI():
+    
+    def getTurn(gamestate):
+        pass
